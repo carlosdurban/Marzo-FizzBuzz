879f422d85c6eced571d51a1345b7d1aa80db2ba
Carlos Durban Antuñano <kaken20@gmail.com>
2012-07-22 20:48:58 +0200

    Commit

---
+++ Marzo-FizzBuzz/FizzBuzzChecker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Marzo_FizzBuzz
+{
+    public class FizzBuzzChecker
+    {
+      
+        public string Check(List<int> numbers)
+        {
+            StringBuilder BizzBuzzBuilder = new StringBuilder();
+
+            foreach (int number in numbers)
+            {
+                if (IsFizzBuzz(number))
+                    BizzBuzzBuilder.Append(number + " FizzBuzz"+ System.Environment.NewLine);
+                else if (IsFizz(number))
+                    BizzBuzzBuilder.Append(number + " Fizz" + System.Environment.NewLine);
+                else if (IsBuzz(number))
+                    BizzBuzzBuilder.Append(number + " Buzz" + System.Environment.NewLine);
+                else
+                    BizzBuzzBuilder.Append(number + System.Environment.NewLine);
+            }
+            return BizzBuzzBuilder.ToString();
+        }
+
+        protected bool IsFizzBuzz(int number)
+        {
+            return number % 3 == 0 && number % 5 == 0;
+        }
+
+        protected bool IsFizz(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        protected bool IsBuzz(int number)
+        {
+            return number % 5 == 0;
+        }
+    }
+}
---
+++ Marzo-FizzBuzzTest/Marzo-FizzBuzzTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+using Marzo_FizzBuzz;
+
+
+namespace Marzo_FizzBuzzTest
+{
+    [TestFixture]
+    public class Marzo_FizzBuzzTest
+    {
+        [Test]
+        public void CheckFizzBuzzNumbers()
+        {
+            FizzBuzzChecker FizzBuzz = new FizzBuzzChecker();
+
+            List<int> Numbers = Enumerable.Range(1, 100).ToList();
+
+            Console.Write(FizzBuzz.Check(Numbers));
+        }
+    }
+}
